529e9a6ee1f2167614e20c877407b2c898e838c3
cngo <Hieu_Ngo@intuit.com>
2020-02-16 16:12:38 -0700

    updated

--- flaskapp/test.py
+++ flaskapp/test.py
@@ -6,7 +6,7 @@ url = 'https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp1'
 payload = {
 		'ip_address1':  '34.215.12.31:5000',
 		'ip_address2':  '54.201.242.61:5000',
-		'load_balancer' :  'Mp1ALB-1220530125.us-west-2.elb.amazonaws.com',
+		'load_balancer' :  'SecondALB-1024867037.us-west-2.elb.amazonaws.com',
 		'submitterEmail':  'hieua0609ptnk+coursera@gmail.com',
 		'secret':  'Vthf44PUaOav3RLq'
 		}
