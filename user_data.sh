f4c144385818e55b29988c00ebf1fb56110927f2
cngo <Hieu_Ngo@intuit.com>
2020-02-16 14:32:42 -0700

    fix

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,7 @@
 sudo apt-get update -y
 sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
 sudo pip3 install flask
+rm -rf cs498_cloud_computing_applications
 git clone "https://github.com/hcngo/cs498_cloud_computing_applications.git"
 cd cs498_cloud_computing_applications/flaskapp
 sudo ln -sT ~/cs498_cloud_computing_applications/flaskapp /var/www/html/flaskapp
