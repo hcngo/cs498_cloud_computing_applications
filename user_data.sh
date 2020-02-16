16651e309097ace678ff460ed9d0b676c1977a75
cngo <Hieu_Ngo@intuit.com>
2020-02-16 14:24:16 -0700

    updated userdata.sh

--- user_data.sh
+++ user_data.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 -y
-sudo pip3 install virtualenv
+sudo pip3 install flask
 git clone "https://github.com/hcngo/cs498_cloud_computing_applications.git"
 cd cs498_cloud_computing_applications/flaskapp
-virtualenv myprojectenv
-source myprojectenv/bin/activate
-pip3 install -r requirements.txt
 sudo ln -sT ~/cs498_cloud_computing_applications/flaskapp /var/www/html/flaskapp
 
 
