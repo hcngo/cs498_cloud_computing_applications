a1028b9d91674071a5b23a89b6e784545f3ab2e1
cngo <Hieu_Ngo@intuit.com>
2020-03-15 22:22:37 -0600

    added server

---
+++ mp3/server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from kubernetes import client, config
+from util import create_free_job_object
+app = Flask(__name__)
+SEED = 0
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+batch_v1 = client.BatchV1Api()
+
+@app.route('/', methods=['GET'])
+def get():
+    global SEED
+    return f'{SEED}'
+
+@app.route('/img-classification/free', methods=['POST'])
+def post():
+    data = request.get_json(True, False, False)
+    dataset = data["dataset"] if "dataset" in data else "mnist"
+    free_job = create_free_job_object(client=client, dataset_name=dataset)
+    api_response = batch_v1.create_namespaced_job(
+        body=free_job,
+        namespace="free-service"
+    )
+    return "Job created. status='%s'" % str(api_response.status)
+  
+if __name__ == '__main__':
+  app.run()
---
+++ mp3/server/util.py
@@ -0,0 +1,34 @@
+def create_free_job_object(client, dataset_name):
+    # Configureate Pod template container
+    job_name = "free"
+    job_namespace = "free-service"
+
+    resources = client.V1ResourceRequirements(
+        limits={"cpu": "0.9"},
+        requests={"cpu": "0.9"}
+    )
+    container_env = [
+        client.V1EnvVar(name="DATASET", value=dataset_name),
+        client.V1EnvVar(name="TYPE", value="ff")
+    ]
+    container = client.V1Container(
+        name=job_name,
+        image="hieungo91/mp3-cloud-computing-applications:latest",
+        resources=resources,
+        env=container_env)
+    # Create and configurate a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": job_name}),
+        spec=client.V1PodSpec(restart_policy="Never", containers=[container]))
+    # Create the specification of deployment
+    spec = client.V1JobSpec(
+        template=template,
+        backoff_limit=4)
+    # Instantiate the job object
+    job = client.V1Job(
+        api_version="batch/v1",
+        kind="Job",
+        metadata=client.V1ObjectMeta(name=job_name, namespace=job_namespace),
+        spec=spec)
+
+    return job
