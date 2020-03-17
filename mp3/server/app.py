bc33a1798f837ed2946421188b2f72c2b07e055a
cngo <Hieu_Ngo@intuit.com>
2020-03-16 22:56:57 -0600

    jfa;d

--- mp3/server/app.py
+++ mp3/server/app.py
@@ -14,7 +14,7 @@ def get():
     return f'{SEED}'
 
 @app.route('/img-classification/free', methods=['POST'])
-def post():
+def free_job():
     data = request.get_json(True, False, False)
     dataset = data["dataset"] if "dataset" in data else "mnist"
     free_job = create_free_job_object(client=client, dataset_name=dataset)
@@ -24,16 +24,16 @@ def post():
     )
     return "Free Job created. status='%s'" % str(api_response.status)
 
-# @app.route('/img-classification/premium', methods=['POST'])
-# def post():
-#     data = request.get_json(True, False, False)
-#     dataset = data["dataset"] if "dataset" in data else "kmnist"
-#     premium_job = create_premium_job_object(client=client, dataset_name=dataset)
-#     api_response = batch_v1.create_namespaced_job(
-#         body=premium_job,
-#         namespace="default"
-#     )
-#     return "Premium Job created. status='%s'" % str(api_response.status)
+@app.route('/img-classification/premium', methods=['POST'])
+def premium_job():
+    data = request.get_json(True, False, False)
+    dataset = data["dataset"] if "dataset" in data else "kmnist"
+    premium_job = create_premium_job_object(client=client, dataset_name=dataset)
+    api_response = batch_v1.create_namespaced_job(
+        body=premium_job,
+        namespace="default"
+    )
+    return "Premium Job created. status='%s'" % str(api_response.status)
 
 
   
